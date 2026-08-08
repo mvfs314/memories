6c2540d0411ee22ba5eab81ba395ee6338cabfff
Marcus <mvfs314@gmail.com>
2026-08-08 00:26:42 -0300

    Update quartz.ts

--- quartz.ts
+++ quartz.ts
@@ -1,11 +1,11 @@
-import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
-
-const config = await loadQuartzConfig()
-export default config
-export const layout = await loadQuartzLayout()
-
 import * as ExternalPlugin from "./.quartz/plugins"
 
 ExternalPlugin.Explorer({
   filterFn: (node) => !node.name?.startsWith("_") && !node.name?.startsWith("."),
 })
+
+import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+
+const config = await loadQuartzConfig()
+export default config
+export const layout = await loadQuartzLayout()
